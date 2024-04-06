a9a80a491bcfe05250b0c2a8d99f185ac9797d74
alstj2384 <ass91160@naver.com>
2024-04-06 15:23:18 +0900

    feat : 시큐리티 설정 추가

---
+++ src/main/java/spring/blog/service/UserService.java
@@ -0,0 +1,23 @@
+package spring.blog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import spring.blog.domain.User;
+import spring.blog.dto.AddUserRequest;
+import spring.blog.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public Long save(AddUserRequest dto){
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
